10d8aa94c209486b14f57a2081163f586bf71cca
MaksymZolotarchuk <146109073+MaksymZolotarchuk@users.noreply.github.com>
2026-04-14 01:15:17 +0300

    Create variables.tf

---
+++ infra/terraform/variables.tf
@@ -0,0 +1,17 @@
+variable "resource_group_location" {
+  type        = string
+  default     = "francecentral"
+  description = "Location of the resource group."
+}
+
+variable "resource_group_name_prefix" {
+  type        = string
+  default     = "rg-devops-lab-v2"
+  description = "Prefix of the resource group name."
+}
+
+variable "admin_username" {
+  type        = string
+  default     = "azureuser"
+  description = "The username for the local administrator account."
+} 
